3e8ae84467c1276d3e1a17a42c346a71cdce9e2b
Berget1411 <107999361+Berget1411@users.noreply.github.com>
2023-08-23 14:12:53 +0200

    add update display when button is pressed

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const currentDisplay = document.querySelector("#current-display");
+const lastDisplay = document.querySelector("#last-display");
+const clearButton = document.querySelector("#clear");
+const deleteButton = document.querySelector("#delete");
+const numAndOperand = document.querySelectorAll("#num-operate button");
 const add = (n1, n2) => {
   return n1 + n2;
 };
@@ -21,3 +26,11 @@ let n2;
 const operate = (operator, n1, n2) => {
   return operator(n1, n2);
 };
+
+const updateDisplay = (e) => {
+  currentDisplay.textContent = e.target.textContent;
+};
+
+numAndOperand.forEach((button) => {
+  button.addEventListener("click", updateDisplay);
+});
